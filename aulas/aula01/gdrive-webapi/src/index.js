2f4bc42bfc1c295439d0fbca182efdb663246d81
wslmacieira <wslmacieira@outlook.com>
2021-09-08 10:25:33 -0300

    instanciando o socket.io

--- aulas/aula01/gdrive-webapi/src/index.js
+++ aulas/aula01/gdrive-webapi/src/index.js
@@ -1,12 +1,13 @@
 import https from 'https';
 import fs from 'fs';
-import { logger } from './logger.js'
+import { logger } from './logger.js';
+import { Server } from 'socket.io'
 
 const PORT = process.env.PORT || 3000;
 
 const localHostSSL = {
-  key: fs.readFileSync('./certificates/key.pem', 'utf-8'),
-  key: fs.readFileSync('./certificates/cert.pem', 'utf-8'),
+  key: fs.readFileSync('./certificates/key.pem'),
+  cert: fs.readFileSync('./certificates/cert.pem'),
 }
 
 const server = https.createServer(
@@ -16,9 +17,18 @@ const server = https.createServer(
   }
 )
 
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    credentials: false
+  }
+})
+
+io.on("connection", (socket) => logger.info(`someone connected ${socket.id}`))
+
 const startServer = () => {
-  const { address, port } = server.address()
-  logger.info(`app runing at https://${address}:${port}`)
+  const { address, port } = server.address();
+  logger.info(`app running at https://${address}:${port}`)
 }
 
 server.listen(PORT, startServer)
