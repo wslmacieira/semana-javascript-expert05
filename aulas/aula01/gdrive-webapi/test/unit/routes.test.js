d3195536d29a403742b10e5cabe1003d4c372124
wslmacieira <wslmacieira@outlook.com>
2021-09-09 20:40:11 -0300

    test handler

--- aulas/aula01/gdrive-webapi/test/unit/routes.test.js
+++ aulas/aula01/gdrive-webapi/test/unit/routes.test.js
@@ -1,4 +1,4 @@
-import { describe, test, expect } from "@jest/globals";
+import { describe, test, expect, jest } from "@jest/globals";
 import Routes from './../../src/routes.js'
 
 describe('#Routes test suite', () => {
@@ -15,7 +15,35 @@ describe('#Routes test suite', () => {
       expect(routes.io).toStrictEqual(ioObj)
     })
   });
-  test('#teste teste', () => {
-    expect(true).toBeTruthy();
-  })
+  describe('#handler', () => {
+    const defaultParams = {
+      request: {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        },
+        method: '',
+        body: {}
+      },
+      response: {
+        setHeader: jest.fn(),
+        writeHeader: jest.fn(),
+        end: jest.fn()
+      },
+      values: () => Object.values(defaultParams)
+    }
+    test('given an inexistent route it should choose default route', () => {
+      const routes = new Routes()
+      const params = {
+        ...defaultParams
+      }
+
+      params.request.method = 'inexistent'
+      routes.handler(...params.values())
+      expect(params.response.end).toHaveBeenCalledWith('hello world')
+    })
+    test.todo('it should set any request woth CORS enabled')
+    test.todo('given method OPTIONS it should choose options route')
+    test.todo('given method POST it should choose options route')
+    test.todo('given method GET it should choose options route')
+  });
 });
