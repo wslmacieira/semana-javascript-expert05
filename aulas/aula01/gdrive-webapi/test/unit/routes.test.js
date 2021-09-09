92a33fb0d9fd983ec6f4d4f9093f55cfdad72427
wslmacieira <wslmacieira@outlook.com>
2021-09-09 15:10:23 -0300

    test socket

---
+++ aulas/aula01/gdrive-webapi/test/unit/routes.test.js
@@ -0,0 +1,21 @@
+import { describe, test, expect } from "@jest/globals";
+import Routes from './../../src/routes.js'
+
+describe('#Routes test suite', () => {
+
+  describe('#setSocketInstance', () => {
+    test('setSocket should store io instance', () => {
+      const routes = new Routes()
+      const ioObj = {
+        to: (id) => ioObj,
+        emit: (event, message) => { }
+      }
+
+      routes.setSocketInstance(ioObj)
+      expect(routes.io).toStrictEqual(ioObj)
+    })
+  });
+  test('#teste teste', () => {
+    expect(true).toBeTruthy();
+  })
+});
